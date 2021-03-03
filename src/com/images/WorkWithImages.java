7900fbc679606ca0cf021dfed55c2932ff03a39e
v.rublova <v.rublova@student.csn.khai.edu>
2021-03-03 18:29:32 +0200

    Implemented single thread execution

--- src/com/images/WorkWithImages.java
+++ src/com/images/WorkWithImages.java
@@ -1,4 +1,43 @@
 package com.images;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class WorkWithImages {
+    static private List<String> urls;
+
+    public static List<String> getUrls() {
+        return urls;
+    }
+
+    static public void WorkWithImages(String filename){
+        try  {
+            urls = Files.readAllLines(Paths.get(filename), Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static public void DownloadImage(String url,String name) throws IOException {
+
+        BufferedImage image = ImageIO.read(new URL(url));
+        ImageIO.write(image, "jpg", new File("output/"+name+".jpg"));
+    }
+    static public void RebaseImage(String s_path,String d_path,String name) throws IOException{
+        BufferedImage image = ImageIO.read(new File(s_path));
+        ImageIO.write(image, "jpg", new File(d_path+"/"+name+".jpg"));
+
+    }
+    static public void CleanDir(String name){
+        File dir = new File(name);
+        for(File file: dir.listFiles())
+            if (!file.isDirectory())
+                file.delete();
+    }
 }
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,8 +1,44 @@
 package com.main;
 
+import com.images.WorkWithImages;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static private ArrayList<String> names = new ArrayList<>() {
+        {
+            add("name_1");
+            add("name_2");
+            add("name_3");
+            add("name_4");
+            add("name_5");
+            add("name_6");
+            add("name_7");
+            add("name_8");
+            add("name_9");
+            add("name_10");
+            add("name_11");
+            add("name_12");
+            add("name_13");
+        }
+    };
+
+    public static void main(String[] args) throws IOException {
+        WorkWithImages.WorkWithImages("image_urls.txt");
+        //single thread download
+        byte i = 0;
+        for (String url : WorkWithImages.getUrls()) {
+            WorkWithImages.DownloadImage(url, names.get(i));
+            i++;
+        }
+        //single thread filesystem
+        i = 0;
+        for (byte j = 0; j < names.size(); j++) {
+            WorkWithImages.RebaseImage("output/" + names.get(i) + ".jpg", "rebase", names.get(i));
+            i++;
+        }
     }
 }
